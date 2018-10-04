0e438adf1e1c0c11f78208b794a75574a4c600cf
akarinS <akaringithub0655@gmail.com>
2018-10-04 11:03:22 +0900

    アカウント機能完成

--- accountwallet.py
+++ accountwallet.py
@@ -1,10 +1,10 @@
-from typing import Any, Callable, List, Tuple
+from typing import Any, Callable, List, Tuple, Union
 import sys
 import os
 import sqlite3
 import time
 from coinrpc import CoinRPC
-from decimal import Decimal
+from decimal import Decimal, ROUND_DOWN
 from config import DBPATH
 from config import RPCUSER, RPCPASSWORD, RPCPORT
 from config import MINCONF
@@ -41,19 +41,21 @@ def init_db(cursor: sqlite3.Cursor) -> None:
     cursor.execute('create table account_wallet(account text unique not null, balance real default 0.0 check(balance >= 0.0) not null)')
     cursor.execute('create table account_address(account text not null, address text unique not null)')
     cursor.execute('create table notified_tx(txid text not null, time integer not null, confirmed integer default 0 not null, account text, value real)')
-    cursor.execute('create table withdrawal_req(account text not null, address text not null, amount real check(amount >= 0.1) not null, completed integer default 0 not null)')
+    cursor.execute('create table withdrawal_req(account text not null, address text not null, amount real check(amount >= 0.01) not null, completed integer default 0 not null)')
 
 @sql_decorator
 def get_new_account_address(cursor: sqlite3.Cursor, account: str) -> str:
-    address = rpc.call('getnewaddress')
-
     cursor.execute('insert or ignore into account_wallet(account) values(?)', (account,))
+
+    address = rpc.call('getnewaddress')
     cursor.execute('insert into account_address(account, address) values(?, ?)', (account, address))
 
     return address
 
 @sql_decorator
 def get_addresses_by_account(cursor: sqlite3.Cursor, account: str) -> List:
+    cursor.execute('insert or ignore into account_wallet(account) values(?)', (account,))
+
     cursor.execute('select address from account_address where account == ?', (account,))
     addresses = [r[0] for r in cursor.fetchall()]
 
@@ -62,19 +64,91 @@ def get_addresses_by_account(cursor: sqlite3.Cursor, account: str) -> List:
 @sql_decorator
 def get_account_balance(cursor: sqlite3.Cursor, account: str) -> Tuple[Decimal, Decimal]:
     cursor.execute('insert or ignore into account_wallet(account) values(?)', (account,))
-    cursor.execute('select balance from account_wallet where account == ?', (account,))
 
+    cursor.execute('select balance from account_wallet where account == ?', (account,))
     balance = Decimal(str(cursor.fetchone()[0]))
 
     cursor.execute('select value from notified_tx where account == ? and confirmed == 0 and time > ?', (account, int(time.time()) - 60 * MINCONF * 10))
     values = [Decimal(str(r[0])) for r in cursor.fetchall()]
 
     confirming_balance = Decimal('0.0')
-
     for value in values:
         confirming_balance = confirming_balance + value
 
     return (balance, confirming_balance)
+
+def is_amount(str_amount: str) -> bool:
+    if str_amount.lower() in {'all', '全額'}:
+        return True
+
+    try:
+        Decimal(str_amount)
+
+    except:
+        return False
+
+    return True
+
+def get_amount(str_amount: str, balance: Decimal) -> Decimal:
+    if str_amount.lower() in {'all', '全額'}:
+        return balance
+
+    return Decimal(str_amount).quantize(Decimal('1e-8'), rounding = ROUND_DOWN)
+
+@sql_decorator
+def move(cursor: sqlite3.Cursor, from_account: str, to_account: str, str_amount: str) -> Union[str, Decimal]:
+    cursor.execute('insert or ignore into account_wallet(account) values(?)', (from_account,))
+    cursor.execute('insert or ignore into account_wallet(account) values(?)', (to_account,))
+
+    if not is_amount(str_amount):
+        return 'wrong'
+
+    cursor.execute('select balance from account_wallet where account == ?', (from_account,))
+    from_balance = Decimal(str(cursor.fetchone()[0]))
+
+    cursor.execute('select balance from account_wallet where account == ?', (to_account,))
+    to_balance = Decimal(str(cursor.fetchone()[0]))
+
+    amount = get_amount(str_amount, from_balance)
+
+    if amount <= 0:
+        return 'few'
+
+    if from_balance < amount:
+        return 'insufficient'
+
+    from_balance = from_balance - amount
+    to_balance = to_balance + amount
+
+    cursor.execute('update account_wallet set balance = ? where account == ?', (float(from_balance), from_account))
+    cursor.execute('update account_wallet set balance = ? where account == ?', (float(to_balance), to_account))
+
+    return amount
+
+@sql_decorator
+def add_withdrawal_request(cursor: sqlite3.Cursor, account: str, address: str, str_amount: str) -> Union[str, Decimal]:
+    cursor.execute('insert or ignore into account_wallet(account) values(?)', (account,))
+
+    if not is_amount(str_amount):
+        return 'wrong'
+
+    cursor.execute('select balance from account_wallet where account == ?', (account,))
+    balance = Decimal(str(cursor.fetchone()[0]))
+
+    amount = get_amount(str_amount, balance)
+
+    if amount <= 0.01:
+        return 'few'
+
+    if balance < amount:
+        return 'insufficient'
+
+    balance = balance - amount
+
+    cursor.execute('update account_wallet set balance = ? where account == ?', (float(balance), account))
+    cursor.execute('insert into withdrawal_req(account, address, amount) values(?, ?, ?)', (account, address, float(amount)))
+
+    return amount
 
 def main() -> None:
     @sql_decorator
@@ -133,7 +207,7 @@ def main() -> None:
         print('check_tx')
         cursor.execute('select txid from notified_tx where confirmed == 0 and time > ?', (int(time.time()) - 60 * MINCONF * 10,))
 
-        txids = set([r[0] for r in cursor.fetchall()])
+        txids = {r[0] for r in cursor.fetchall()}
 
         if len(txids) == 0:
             print('no transaction')
@@ -174,8 +248,56 @@ def main() -> None:
 
             cursor.execute('update notified_tx set confirmed = 1 where txid == ?', (txid,))
 
-    def exec_withdrawal() -> None:
-        pass
+    @sql_decorator
+    def exec_withdrawal(cursor: sqlite3.Cursor) -> None:
+        print()
+        print('exec_withdrawal')
+        cursor.execute('select address from withdrawal_req where completed == 0')
+        addresses = {r[0] for r in cursor.fetchall()}
+
+        if len(addresses) == 0:
+            print('no request')
+            return
+
+        params = {}
+
+        for address in addresses:
+            cursor.execute('select amount from withdrawal_req where address == ?', (address,))
+            req_amounts = [Decimal(str(r[0])) for r in cursor.fetchall()]
+
+            amount = Decimal('0.0')
+            for req_amount in req_amounts:
+                amount = amount + req_amount
+
+            params[address] = float(amount)
+            print(f'  address: {address}, amount: {amount}')
+
+        print(' sending')
+        try:
+            txid = rpc.call('sendmany', '', params, 1, '', list(addresses))
+
+        except:
+            txid = None
+
+        if txid is None:
+            print('  failed')
+            cursor.execute('select account, value from withdrawal_req where completed == 0')
+            for req in cursor.fetchall():
+                account = req[0]
+                value = Decimal(str(req[1]))
+
+                cursor.execute('select balance from account_wallet where account == ?', (account,))
+                balance = Decimal(str(cursor.fetchone()[0]))
+
+                balance = balance + value
+
+                cursor.execute('update account_wallet set balance = ? where account == ?', (float(balance), account))
+
+            cursor.execute('update withdrawal_req set completed = -1 where completed == 0')
+            return
+
+        print('  success')
+        cursor.execute('update withdrawal_req set completed = 1 where completed == 0')
 
     if len(sys.argv) < 2:
         print('Argument is missing.')
